fdf4babcef76b4faf6a1a03f68e1b48a6d5af97c
Дмитрий <konchakov.d.s@yandex.ru>
2023-08-03 11:48:40 +0300

    dom 3 (book-reader) completed

---
+++ dom/book-reader/task.js
@@ -0,0 +1,27 @@
+const book = document.querySelector('.book');
+const controls = document.querySelector('.book__control');
+
+for (let control of controls.children) {
+	control.addEventListener('click', fontChange);
+}
+
+function fontChange(ev) {
+	ev.preventDefault();
+
+	if (ev.target.dataset.size === 'big') {
+		book.classList.add('book_fs-big');
+		book.classList.remove('book_fs-small');
+	} else if (ev.target.dataset.size === 'small') {
+		book.classList.add('book_fs-small');
+		book.classList.remove('book_fs-big');
+	} else {
+		book.classList.remove('book_fs-big');
+		book.classList.remove('book_fs-small');
+	}
+
+	for (let letter of document.querySelectorAll('.font-size')) {
+		letter.classList.remove('font-size_active');
+	}
+
+	ev.terget.classList.add('font-size_active');
+}
