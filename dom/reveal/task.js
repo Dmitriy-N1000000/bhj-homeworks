8de13238103a883dcd5f2a6dfc0aa9e83f70b422
Дмитрий <konchakov.d.s@yandex.ru>
2023-08-03 11:42:42 +0300

    dom 1 (reveal) completed

---
+++ dom/reveal/task.js
@@ -0,0 +1,13 @@
+const reveals = document.getElementsByClassName('reveal');
+
+window.addEventListener('scroll', () => {
+	for (const reveal of reveals) {
+		const { innerHeight } = window;
+		const { top } = reveal.getBoundingClientRect();
+		if (top < innerHeight && top > 0) {
+			reveal.classList.add('reveal_active');
+		} else {
+			reveal.classList.remove('reveal_active');
+		}
+	}
+})
